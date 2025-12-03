b0bc4c43209bab55287bc91d95b0e22d8b1330ca
Tony <andrianjaka.tony@gmail.com>
2025-12-03 16:54:57 +0300

    feat: learning objects transformation

--- src/script.js
+++ src/script.js
@@ -12,15 +12,40 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene();
 
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+// Axes helper
+const axesHelper = new THREE.AxesHelper(8);
+scene.add(axesHelper);
+
+// Group
+const group = new THREE.Group();
+scene.add(group);
+group.position.x = 1;
+group.rotation.y = 1;
+
+// Objects
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+);
+cube2.position.x = -1.2;
+group.add(cube2);
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ffff, wireframe: true })
+);
+cube3.position.x = 1.2;
+group.add(cube3);
 
 // Camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.set(0.5, 0.5, 3);
 scene.add(camera);
 
 // Renderer
